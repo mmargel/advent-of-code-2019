b70a15c6749b5110db7e576b768f78bb76e79fe6
mmargel <michael.margel@shopify.com>
2019-12-05 16:10:47 -0500

    Misc cleanup

--- day-2/puzzle.rb
+++ day-2/puzzle.rb
@@ -7,7 +7,7 @@ def preprocess_1(data)
   data
 end
 
-def puzzle_1
+def part_1
   tape = preprocess_1(input).dup
   tape
     .each_slice(4)
@@ -27,12 +27,16 @@ def puzzle_1
   tape[0]
 end
 
+# Setup for part 2. We need to calculate A dn B, so we set the 
+# values of data[1] and data[2] to those algebraic constructs.
 def preprocess_2(data) 
   data[1] = {A: 1, B: 0, C: 0 }
   data[2] = {A: 0, B: 1, C: 0 }
   data
 end
 
+# Wherever possible, we want to return an integer instead of an
+# algebraic construct. This makes it easier to manipulate later.
 def try_make_int(input)
   return input if input.is_a?(Integer)
   return input[:A] == 0 && input[:B] == 0 ? input[:C] : input
@@ -49,6 +53,8 @@ def add(a, b)
   })
 end
 
+# A basic multiplier. This implicitly assumes that the final
+# equation will be linear. If it's quadratic, this will fail.
 def multiply(s, a)
   if a.is_a?(Integer) && s.is_a?(Integer)
     s * a
@@ -63,7 +69,7 @@ def multiply(s, a)
   end
 end
 
-def puzzle_2
+def part_2
   tape = preprocess_2(input).dup
   tape
     .each_slice(4)
@@ -89,7 +95,7 @@ def puzzle_2
 end
 
 # 7210630
-# puts puzzle_1
+puts part_1
 
 # 480_000*A + B + 1450628 = 19690720
 # 480_000*A + B = 18240092
@@ -97,4 +103,5 @@ end
 # A = 38
 # B = 92
 # {:A=>480000, :B=>1, :C=>1450628}
-puts puzzle_2
+puts part_2
+puts({noun: (19_690_720 - 1_450_628) / 480_000, verb: (19_690_720 - 1_450_628) % 480_000})
