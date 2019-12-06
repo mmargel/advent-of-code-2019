c299e4e39c5c91eee2a977967299ad0dd25589af
mmargel <michael.margel@shopify.com>
2019-12-05 19:27:10 -0500

    Finished Day 5, golfed it

--- day-5/puzzle.rb
+++ day-5/puzzle.rb
@@ -1,68 +1,84 @@
 require_relative 'input'
 
-class Tape
-  def initialize(values)
-    @values = values.dup
-  end
-
-  # mode 0: position
-  # mode 1: immediate
-  def get(index, mode)
-    mode = mode.to_i if mode.is_a?(String)
-    (mode == 0) ? @values[index] : index
-  end
-
-  def put(index, value)
-    @values[index] = value
-  end
-
-  def[](index)
-    @values[index]
+# We extend Array because we want to use most of that functionality.
+class Tape < Array
+  # This is the tidiest way to fetch a value by mode or
+  # by reference. Here, 0 --> reference, since that's our "default"
+  # (sort of, but it makes the code cleaner if we treat it like it is).
+  def[](index, call_by_reference = 0)
+    call_by_reference == 0 ? super(index) : index
   end
 end
 
-def part_1
-  tape = Tape.new(input)
+# This is a clean way of storing information about our various commands.
+def commands
+  @commands = {
+    1 => {:word => :add, :inc => 4},
+    2 => {:word => :mult, :inc => 4},
+    3 => {:word => :in, :inc => 2},
+    4 => {:word => :out, :inc => 2},
+    5 => {:word => :jnz, :inc => 0},
+    6 => {:word => :jz, :inc => 0},
+    7 => {:word => :lt, :inc => 4},
+    8 => {:word => :eq, :inc => 4},
+    99 => {:word => :halt},
+  }
+end
+
+# We take an arugment to mock the input, so we don't need to manually enter it.
+def run(mock_input)
+  @tape = Tape.new(input)
+
+  # We don't need this, but it makes the code a bit tidier.
+  def t(a, b); @tape[a, b]; end
+
+  # PC = program counter
   pc = 0
+  
+  # We'll keep looping until we reach a halt. Based on the problem,
+  # all well-formed programs will have an explicit halt command.
   while true
-    instruction = tape[pc]
-    opcode = instruction % 100
-    mode = sprintf('%03d', instruction / 100)
-    # puts "Step ##{pc}: #{mode} (#{opcode})"
-    case opcode
-    when 1
-      # [instr, A, B, A + B]
-      in_1 = tape.get(tape[pc+1], mode[2])
-      in_2 = tape.get(tape[pc+2], mode[1])
-      out =  in_1 + in_2
-      tape.put(tape[pc+3], out)
-      pc += 4
-    when 2
-      # [instr, A, B, A * B]
-      in_1 = tape.get(tape[pc+1], mode[2])
-      in_2 = tape.get(tape[pc+2], mode[1])
-      out = in_1 * in_2
-      tape.put(tape[pc+3], out)
-      pc += 4
-    when 3
-      # [instr, A]
-      puts "Waiting for input: "
-      input = gets.chomp.to_i
-      tape.put(tape[pc+1], input)
-      pc += 2
-    when 4
-      # [instr, A]]
-      puts "OUTPUT: #{tape.get(tape[pc+1], mode[2])}"
-      pc += 2
-    when 99
-      puts "HALT"
-      return
-    else
-      puts "ERROR at #{pc}: #{opcode}"
-      return
+    # Gets the instruction, opcode, mode, and keyword for the current step.
+    instruction = @tape[pc]
+    opcode, mode = [instruction % 100, instruction / 100]
+    command = commands[opcode]
+
+    # Quick check to see if we hit an error.
+    # We could do this below, but I'd rather make this check explicit here.
+    return puts "ERROR at #{pc}: #{opcode}" unless command
+
+    # Shorthand for the arguments and reference flags.
+    # I'm okay with this approach, since we have 3 or fewer for all instructions.
+    # 
+    # Since we always have (at most) 2 inputs and the output is never by
+    # reference, we can use r0 and r1 for the flags.
+    a0, a1, a2 = [@tape[pc+1], @tape[pc+2], @tape[pc+3]]
+    r0, r1 = [mode & 1, mode & 2]
+    
+    # Each command maps to a symbol (listed above).
+    # For each command, we use our custom tape[] method, which lets us
+    # easily specify whether it's by reference or by value.
+    # 
+    # To make by-reference checks easier, we use bit masks to check the mode.
+    #   e.g. if input 2 is by reference, it would by X1X which maps to N!=0
+    #   since X1X & 010 (2) == 010 != 0
+    case command[:word]
+      when :add then @tape[a2] = t(a0, r0) + t(a1, r1)
+      when :mult then @tape[a2] = t(a0, r0) * t(a1, r1)
+      when :in then @tape[a0] = mock_input
+      when :out then puts "OUTPUT: #{t(a0, r0)}"
+      when :jnz then pc = (t(a0, r0) != 0) ? t(a1, r1) : pc + 3
+      when :jz then pc = (t(a0, r0) == 0) ? t(a1, r1) : pc + 3
+      when :lt then @tape[a2] = (t(a0, r0) < t(a1, r1)) & 1
+      when :eq then @tape[a2] = (t(a0, r0) == t(a1, r1)) & 1 
+      when :halt then return puts "HALT"
     end
+    pc += command[:inc]
   end
 end
 
-# 7566643
-puts part_1
+# 1: 7566643
+puts run(1)
+
+# 5: 9265694
+puts run(5)
